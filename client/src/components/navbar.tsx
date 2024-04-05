0b4672368d42cead0ba3064442963e0490b5c82f
Mangalam Raj <mango.26june@gmail.com>
2024-04-05 20:22:31 +0530

    navbar fix

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./modeToggler"
 const Navbar = () =>{
     return(
-        <div className="sticky top-0 z-50  h-16 w-full items-center backdrop-blur shadow-sm container flex justify-between">
+        <div className="sticky top-0 z-50  h-16 w-full items-center backdrop-blur shadow-sm md:container px-2 flex justify-between">
             <div className="font-semibold text-xl md:text-2xl">
             🪐EventHorizon
             </div>
