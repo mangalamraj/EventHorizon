72da49aec9d5e300121b15378e8a15b10812ebac
Mangalam Raj <mango.26june@gmail.com>
2024-04-09 16:11:55 +0530

    navbar changed

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () =>{
     return(
         <div className="sticky top-0 z-50  h-16 w-full items-center backdrop-blur shadow-sm md:container px-2 flex justify-between">
             <div className="font-semibold text-xl md:text-2xl">
-            🪐EventHorizon
+            Mango
             </div>
             <div className="flex justify-between gap-4">
                 <ModeToggle/>
