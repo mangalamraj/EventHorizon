b0e8a97dc0db04461fb88d3ac85e05e4933ee62c
Mangalam Raj <mango.26june@gmail.com>
2024-04-14 04:00:55 +0530

    server setup

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,16 +1,17 @@
-import { Button } from "@/components/ui/button"
-import { ModeToggle } from "./modeToggler"
-const Navbar = () =>{
-    return(
-        <div className="sticky top-0 z-50  h-16 w-full items-center backdrop-blur shadow-sm md:container px-2 flex justify-between">
-            <div className="font-semibold text-xl md:text-2xl">
-            🪐EventHorizon
-            </div>
-            <div className="flex justify-between gap-4">
-                <ModeToggle/>
-                <Button variant="outline">Login</Button>
-            </div>
-        </div>
-    )
-}
-export default Navbar
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./modeToggler";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 z-50  h-16 w-full items-center backdrop-blur shadow-sm md:container px-2 flex justify-between">
+      <div className="font-semibold text-xl md:text-2xl">🪐EventHorizon</div>
+      <div className="flex justify-between gap-4">
+        <ModeToggle />
+        <Button variant="outline">
+          <Link href="/login">Login</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
